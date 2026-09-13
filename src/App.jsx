cbf18b8332cb106c93b4f318e4c507fe51db8503
alicebakeer <alicebakeer2021@gmail.com>
2026-09-13 18:23:32 +0300

    Add content management design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-
 import Home from './Home'
 import Login from './Login'
 import About from './About'
 import Contact from './Contact'
 import Content from './Content'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 function App() {
   return (
 <BrowserRouter basename="/Company">
